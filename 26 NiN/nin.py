635cc469ece6778d0f2b735e7f3535292248f032
Dewsand <58612430+Dewsand@users.noreply.github.com>
2022-02-28 22:04:55 +0800

    Add files via upload

---
+++ 26 NiN/nin.py
@@ -0,0 +1,40 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+def nin_block(in_channels, out_channels, kernel_size, strides, padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels,out_channels,kernel_size,strides,padding),
+        nn.ReLU(),
+        nn.Conv2d(out_channels,out_channels,kernel_size=1),
+        nn.ReLU(),
+        nn.Conv2d(out_channels,out_channels,kernel_size=1),
+        nn.ReLU()
+    )
+
+net = nn.Sequential(
+    nin_block(1,96,kernel_size=11,strides=4,padding=0),
+    nn.MaxPool2d(3,stride=2),
+    nin_block(96,256,kernel_size=5,strides=1,padding=2),
+    nn.MaxPool2d(3,stride=2),
+    nin_block(256,384,kernel_size=3,strides=1,padding=1),
+    nn.MaxPool2d(3,stride=2),
+    nn.Dropout(0.5),
+    nin_block(384,10,kernel_size=3,strides=1,padding=1),
+    nn.AdaptiveAvgPool2d((1,1)),
+    nn.Flatten()
+)
+
+# X = torch.randn(1,1,224,224)
+# for layer in net:
+#     X = layer(X)
+#     print(layer.__class__.__name__, 'output shape:\t',X.shape)
+
+
+
+lr, num_epochs, batch_size = 0.1, 10, 128
+train_iter,test_iter = d2l.load_data_fashion_mnist(batch_size,resize=224)
+d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
+d2l.plt.savefig("nin.png")
+d2l.plt.show()
+
